3ca1bab5278f3c2544d16be5a926955e4cb348f5
arekCodez <arekp0916@gmail.com>
2025-03-04 21:29:44 -0600

    Failed LED Code

--- Techathon/src/main.cpp
+++ Techathon/src/main.cpp
@@ -7,12 +7,16 @@ void setup() {
 }
 
 void loop() {
-  for (int brightness = 0; brightness <= 255; brightness++) { 
-    analogWrite(9, brightness); // Set LED brightness (0-255)
-    delay(10); // Small delay for smooth transition
-  }
-  for (int brightness = 255; brightness >= 0; brightness--) { 
-    analogWrite(9, brightness); // Dim the LED down
-    delay(10);
-  }
+  
+  
+    for (int brightness = 0; brightness <= 255; brightness++) { 
+      analogWrite(9, brightness); // Set LED brightness (0-255)
+      delay(10); // Small delay for smooth transition
+    }
+    for (int brightness = 255; brightness >= 0; brightness--) { 
+      analogWrite(9, brightness); // Dim the LED down
+      delay(10);
+    }
+  
+  
 }
