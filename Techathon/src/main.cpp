d295a864d6bb655de092626a3f8d7b090fef7a31
arekCodez <arekp0916@gmail.com>
2025-03-07 02:52:01 -0600

    no indexOf!!!

--- Techathon/src/main.cpp
+++ Techathon/src/main.cpp
@@ -10,7 +10,9 @@ const byte buzzerPin = 8; // buzzer
 
 String inWord = "";
 String Password = "Pass";
+String fullWord = "";
 bool passAccept = false;
+bool wordDone = false;
 
 SoftwareSerial BTSerial(rxPin, txPin);  // Create a SoftwareSerial object for Bluetooth communication
 
@@ -87,31 +89,63 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop()
-{
+
+void parser(String fullInput){
+  String helperWord = "";
+  
+  for(int i =0; i<fullInput.length(); i++){
+
+    if (fullInput.charAt(i) == ' ' || fullInput.charAt(i)  == '\n' || fullInput.charAt(i)  == '\r' || fullInput.charAt(i) == '!') {
+      if (helperWord.length() > 0) {
+        wordChecker(helperWord);
+        helperWord = ""; // Clear the fullInput for the next one
+      }
+    }
+    else {
+      helperWord += fullInput.charAt(i); // Add character to the fullInput
+    }
+  }
+
+  fullInput = "";
+  wordDone = false;
+}
+
+
+
+
+void btHandle() {
   // Check if we have connection with the Bluetooth device
-  if (BTSerial.available()) {
+  while(BTSerial.available()) {
     // Bluetooth module is reading data and check for inWord
     char c = BTSerial.read();
-
     // Check for end of inWord (space or newline)
-    if (c == ' ' || c == '\n' || c == '\r') {
+    // changing this to "!" to end, literally type "!" at end of command
+    if (c == '!') {
       if (inWord.length() > 0) {
         Serial.print("Received Word: ");
         Serial.println(inWord);
-        wordChecker(inWord);
-        inWord = ""; // Clear the inWord for the next one
+        inWord += " ";
+        fullWord = inWord;
+        inWord = "";
+        wordDone = true;
+        //inWord = ""; // Clear the inWord for the next one
       }
+      return;
     }
     else {
       inWord += c; // Add character to the inWord
     }
   }
+}
 
-  // Check if data is available from the Serial Monitor
-  if (Serial.available()) {
-    // Read the data from the Serial Monitor and send it to the Bluetooth device
-    char c = Serial.read();  // Read the character from the Serial Monitor
-    BTSerial.print(c);  // Send the character to the Bluetooth device
-  }
+
+void loop()
+{
+// new func call with full string
+btHandle();
+
+if(wordDone){
+  parser(fullWord);
+}
+ 
 }
