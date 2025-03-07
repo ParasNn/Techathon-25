8bc81bf7bb458355c741673144a5370f441c4d25
ParasNn <40841618+ParasNn@users.noreply.github.com>
2025-03-06 20:18:47 -0600

    LED control from iphone

--- Techathon/src/main.cpp
+++ Techathon/src/main.cpp
@@ -4,13 +4,16 @@
 // Setup for IO pins -- BTSerial
 const byte rxPin = 10;  // RX pin of HC-05
 const byte txPin = 11;  // TX pin of HC-05
+const int ledPin = 13;
+
 SoftwareSerial BTSerial(rxPin, txPin);  // Create a SoftwareSerial object for Bluetooth communication
 
 void setup()
 {
-  // Pin setup for Bluetooth
+  // Pin setup for IO
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
   // Begin Bluetooth serial communication at 9600 baud rate
   BTSerial.begin(9600);
@@ -23,10 +26,18 @@ void loop()
   // Check if data is available from the Bluetooth device
   if (BTSerial.available()) {
     // Read the data from Bluetooth and send it to the Serial Monitor
-    char c = BTSerial.read();
-    Serial.print(c);
+    int c = BTSerial.read();
+    //turn on LED based on phone input
+    if (c == 1) {
+      digitalWrite(ledPin, 1);
+      Serial.println("led on\n");
+    }
+    else if (c == 0) {
+      digitalWrite(ledPin, 0);
+      Serial.println("led off\n");
+    }
   }
-  
+
   // Check if data is available from the Serial Monitor
   if (Serial.available()) {
     // Read the data from the Serial Monitor and send it to the Bluetooth device
