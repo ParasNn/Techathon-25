99ed58690e3bc24a9e5916270a1507d9e28602ab
arekCodez <arekp0916@gmail.com>
2025-03-08 17:31:56 -0600

    BT->NANO->SERVO

--- NANO SERVO/src/main.cpp
+++ NANO SERVO/src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
+
 
 const byte ledPin = 13; 
 const byte switchPin = 3; 
+const byte rxPin = 10;    // RX pin of HC-05
+const byte txPin = 11;    // TX pin of HC-05
 
 
 Servo myServo;  // Create a Servo object
 int flipper = 180;
+
+SoftwareSerial BTSerial(rxPin, txPin);  // Create a SoftwareSerial object for Bluetooth communication
 
 const int debounceDelay = 50;  // Debounce delay in milliseconds
 unsigned long lastDebounceTime = 0;
@@ -20,19 +26,36 @@ void setup() {
    
     pinMode(switchPin, INPUT);
     pinMode(ledPin, OUTPUT);
+    pinMode(rxPin, INPUT);
+    pinMode(txPin, OUTPUT);
 
+    BTSerial.begin(9600);
+    Serial.begin(9600);
 }
 
+String myWord = "";
 void loop() {
 
-    int pinState = digitalRead(switchPin);
 
+    while(BTSerial.available()) {
+    // Bluetooth module is reading data and check for inmyWord
 
+            Serial.print("test\n");
+            myWord = BTSerial.readStringUntil(' ');
+        
+    }
+
+    //int pinState = digitalRead(switchPin);
+
+/*
     // If the button state has changed, reset the debounce timer
     if (pinState != lastButtonState) {
-        lastDebounceTime = millis();  // Set the timer to the current time
-}
+        lastDebounceTime = millis(); } // Set the timer to the current time
+        */
 
+
+
+/*
 if ((millis() - lastDebounceTime) > debounceDelay) {
     if(pinState == HIGH){
         digitalWrite(ledPin, HIGH); 
@@ -45,6 +68,16 @@ if ((millis() - lastDebounceTime) > debounceDelay) {
     }
 }
 lastButtonState = pinState;
-
+*/
+if(myWord.equals("Go")){
+    digitalWrite(ledPin, HIGH); 
+    myServo.write(flipper); // Move to 180 degrees
+    delay(2000);
+    // Toggle between 180 and 0
+    flipper = (flipper == 180) ? 0 : 180;
+    myWord = "";
+}else{
+    digitalWrite(ledPin, LOW);
+}
    
 }
