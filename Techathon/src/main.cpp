f04b6e1991c99459fdb72e1088d4f0706be60eed
ParasNn <40841618+ParasNn@users.noreply.github.com>
2025-03-08 17:27:47 -0600

    working single module uno code

--- Techathon/src/main.cpp
+++ Techathon/src/main.cpp
@@ -3,8 +3,6 @@
 #include <String.h>
 
 // Setup for IO pins -- BTSerial
-const byte rxPin2 = 8;
-const byte txPin2 = 9;
 const byte rxPin = 10;    // RX pin of HC-05
 const byte txPin = 11;    // TX pin of HC-05
 const byte ledPin = 13;   // Board LED
@@ -12,14 +10,12 @@ const byte buzzerPin = 7; // buzzer
 
 String inWord = "";
 String Password = "Pass";
-String fullWord = "";
 bool passAccept = false;
 
 String commands[10];
 int commPtr = 0;
 
 SoftwareSerial BTSerial(rxPin, txPin);  // Create a SoftwareSerial object for Bluetooth communication
-SoftwareSerial BT2(rxPin2, txPin2);
 
 void passChecker(String wordIn)
 {
@@ -87,47 +83,20 @@ void setup()
   // Pin setup for IO
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  pinMode(rxPin2, INPUT);
-  pinMode(txPin2, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
   // Begin Bluetooth serial communication at 9600 baud rate
   BTSerial.begin(9600);
-  BT2.begin(9600);
   // Begin the regular serial communication at 9600 baud rate (for debugging)
   Serial.begin(9600);
 }
 
-
-void parser(String fullInput){
-  String helperWord = "";
-  
-  for(int i =0; i<fullInput.length(); i++){
-
-    if (fullInput.charAt(i) == ' ' || fullInput.charAt(i)  == '\n' || fullInput.charAt(i)  == '\r' || fullInput.charAt(i) == '!') {
-      if (helperWord.length() > 0) {
-        wordChecker(helperWord);
-        helperWord = ""; // Clear the fullInput for the next one
-      }
-    }
-    else {
-      helperWord += fullInput.charAt(i); // Add character to the fullInput
-    }
-  }
-
-  fullInput = "";
-  wordDone = false;
-}
-
-
-
-
-void btHandle() {
+void loop()
+{
   // Check if we have connection with the Bluetooth device
-  while(BTSerial.available()) {
+  if (BTSerial.available()) {
     // Bluetooth module is reading data and check for inWord
-    Serial.print("test");
     char c = BTSerial.read();
 
     if (c == '.') {
@@ -145,7 +114,6 @@ void btHandle() {
         commands[commPtr++] = inWord;
         inWord = ""; // Clear the inWord for the next one
       }
-      return;
     }
     else {
       inWord += c; // Add character to the inWord
