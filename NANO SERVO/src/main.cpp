74cbe313583938499b9924ec3e47fcd6bf01449b
BrianAi <brian942608@gmail.com>
2025-03-16 18:19:02 -0500

    -uno updated code

--- NANO SERVO/src/main.cpp
+++ NANO SERVO/src/main.cpp
@@ -7,6 +7,7 @@ const byte ledPin = 13;
 const byte switchPin = 3; 
 const byte rxPin = 10;    // RX pin of HC-05
 const byte txPin = 11;    // TX pin of HC-05
+const byte statePin = 2;
 
 
 Servo myServo;  // Create a Servo object
@@ -19,11 +20,31 @@ unsigned long lastDebounceTime = 0;
 int lastButtonState = LOW;
 
 
+void pairing(){
+    Serial.println("Sending AT command....");
+
+    //Send AT commands
+    //int stateValue = digitalRead(statePin);
+    //while(digitalRead(statePin) == LOW){
+        BTSerial.write("AT\r\n");
+        delay(500);
+        Serial.println(BTSerial.readString());
+        delay(500);
+    //}
+    
+
+    // BTSerial.write("AT+STATE\r\n");
+    // delay(500);
+    // Serial.println(BTSerial.readString());
+    // delay(500);
+}
+
 void setup() {
     myServo.attach(9);  // Attach the servo to pin D9
-    myServo.write(90);  // Start at neutral position
+    //myServo.write(90);  // Start at neutral position
     delay(500); 
-   
+
+    pinMode(statePin, INPUT);
     pinMode(switchPin, INPUT);
     pinMode(ledPin, OUTPUT);
     pinMode(rxPin, INPUT);
@@ -31,17 +52,39 @@ void setup() {
 
     BTSerial.begin(9600);
     Serial.begin(9600);
-    delay(500);
+    //delay(500);
+    //pairing();
+    //Serial.println("End of AT command");
     BTSerial.listen();
 }
 
 String myWord = "";
+bool lock = true;
 void loop() {
+    //digitalWrite(ledPin, HIGH); // Turn ON LED
+    //delay(500);
+    if((digitalRead(statePin) == HIGH)  && lock){
+        digitalWrite(ledPin, HIGH);
+        myServo.write(flipper); // Move to 180 degrees
+        // Toggle between 180 and 0
+        flipper = 0;
+        lock = false;
+    }else if((digitalRead(statePin) == LOW) && (!lock)){
+        digitalWrite(ledPin, LOW);
+        myServo.write(flipper); // Move to 0 degrees
+        // Toggle between 180 and 0
+        flipper = 180;
+        lock = true;
+    }
+    //flipper = lock ? 180 : 0;
+    //myServo.write(flipper); 
 
     while(BTSerial.available()) {
     // Bluetooth module is reading data and check for inmyWord
-        Serial.print("test\n");
-        myWord = BTSerial.readStringUntil(' ');
+        
+        myWord = BTSerial.readString();
+        Serial.println(myWord);
+        BTSerial.println("fuck you master");
     }
 
    // int pinState = digitalRead(switchPin);
@@ -70,16 +113,18 @@ lastButtonState = pinState;
 */
 
 
-if(myWord.equals("Go")){
-    digitalWrite(ledPin, HIGH); 
-    myServo.write(flipper); // Move to 180 degrees
-    Serial.println("testing");
-    delay(2000);
-    // Toggle between 180 and 0
-    flipper = (flipper == 180) ? 0 : 180;
-    myWord = "";
-}else{
-    digitalWrite(ledPin, LOW);
-}
+// if(myWord.equals("GO")){
+//     digitalWrite(ledPin, HIGH); // Turn ON LED
+//     delay(500);               // Wait for 1 second
+    
+//     myServo.write(flipper); // Move to 180 degrees
+//     Serial.println("testing");
+//     delay(1000);
+//     // Toggle between 180 and 0
+//     flipper = (flipper == 180) ? 0 : 180;
+//     myWord = "";
+// }else{
+//     digitalWrite(ledPin, LOW);
+// }
    
 }
--- Techathon/src/main.cpp
+++ Techathon/src/main.cpp
@@ -8,6 +8,7 @@ const byte rxPin = 10;    // RX pin of HC-05
 const byte txPin = 11;    // TX pin of HC-05
 const byte ledPin = 13;   // Board LED
 const byte buzzerPin = 7; // buzzer
+const byte masterPowerPin = 6; // power the master at09
 // Second AT09 (Master)
 const byte rxPin2 = 8;     // RX pin of AT09 (Master)
 const byte txPin2 = 9;     // TX pin of AT09 (Master)
@@ -16,7 +17,9 @@ String inWord = "";
 String Password = "Pass";
 String command = "";
 bool passAccept = false;
+bool firstTime = true;
 int commPtr = 0;
+int searchingIndex = -1;
 
 SoftwareSerial BTSerial(rxPin, txPin);  // Create a SoftwareSerial object for Bluetooth communication
 SoftwareSerial BTSerial2(rxPin2, txPin2); // Second AT09 (Master)
@@ -87,11 +90,43 @@ class LinkedList {
   
 LinkedList commands;
 
+int searching(String target){
+  target = "78A50457E9D6";
+  //Send AT commands
+  BTSerial2.listen();
+  BTSerial2.write("AT+INQ\r\n"); // 78A50457E9D6
+  delay(150);
+  String bList = BTSerial2.readString();
+  int index = (bList.charAt(bList.indexOf(target) - 4)) - 0x30;
+  Serial.println(bList);
+
+  while(index <= 0){
+    BTSerial2.write("AT+INQ\r\n"); // 78A50457E9D6
+    delay(150);
+    bList = BTSerial2.readString();
+    index = (bList.charAt(bList.indexOf(target) - 4)) - 0x30;
+    Serial.println(bList);
+    Serial.println(index);
+  }
+  return index;
+}
+
+void pairing(int index){
+  Serial.println("begin pairing");
+  BTSerial2.listen();
+  String temp = "AT+CONN";
+  temp += index;
+  BTSerial2.println(temp); 
+  delay(300);
+  Serial.println(BTSerial2.readString());
+}
 
 void passChecker(String wordIn)
 {
   if (wordIn.equals(Password)) {
     passAccept = true;
+    pairing(searchingIndex);
+    BTSerial.listen();
     BTSerial.print("Main");
     return;
   }
@@ -135,19 +170,19 @@ bool lightTool(String wordIn, bool lightMode)
 void wordChecker(String wordIn)
 {
   static bool lightMode = false;
-
+  if(firstTime){
+    digitalWrite(masterPowerPin, 1);
+    delay(100);
+    searchingIndex = searching((String)"78A50457E9D6");
+    firstTime = false;
+    BTSerial.listen();
+  }
+    
   if (passAccept) {
     if (wordIn.equals("Exit")) {
       passAccept = false;
-      return;
-    }
-    if (wordIn.equals("GO")) {
-      Serial.println("Master to Slave");
-      BTSerial2.listen();
-      delay(500);
-      BTSerial2.write("GO \r\n");
-      delay(500);
-      BTSerial.listen();
+      firstTime = true;
+      digitalWrite(masterPowerPin, 0);
       return;
     }
     lightMode = lightTool(wordIn, lightMode);
@@ -159,88 +194,8 @@ void wordChecker(String wordIn)
 
 }
 
-void setATCommands(){
-  Serial.println("Sending AT command....");
-
-  //Send AT commands
-  BTSerial2.write("AT\r\n");
-  delay(500);
-  Serial.println(BTSerial2.readString());
-  delay(500);
-  BTSerial2.write("AT+INQ\r\n"); // 78A50457E9D6
-  delay(1000);
-  String bList = BTSerial2.readString();
-  delay(1000);
-  String toFind = "0x78A50457E9D6";
-  int index = (bList.charAt(bList.indexOf(toFind) - 2)) - 0x30;
-  Serial.print(bList);
-  delay(500);
-  while(index <= 0){
-    Serial.println("Device not found... Searching again");
-    delay(500);
-    BTSerial2.write("AT+INQ\r\n"); // 78A50457E9D6
-    delay(1000);
-    bList = BTSerial2.readString();
-    delay(500);
-    index = (bList.charAt(bList.indexOf(toFind) - 2)) - 0x30;
-    Serial.print(bList);
-    delay(500);
-  }
-  String temp = "AT+CONN";
-  temp += index;
-  BTSerial2.println(temp); 
-  delay(500);
-  Serial.println(BTSerial2.readString());
-  // Check for response from the Bluetooth module
-  delay(500);
-  Serial.println("Command sent and response checked.");
-  delay(500);
-  Serial.println("running normal program");
-  delay(500);
-
-
-}
-
-void setup()
-{
-  // Pin setup for IO
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  //pinMode(rxPin2, INPUT);
-  //pinMode(txPin2, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-
-  // Begin the regular serial communication at 9600 baud rate (for debugging)
-  Serial.begin(9600);
-  delay(200);
-  BTSerial.begin(9600); // Now start Bluetooth communication
-  BTSerial2.begin(9600);
-  delay(1000);
-  // Print to Serial Monitor to check that the program is running
-  setATCommands();
-  BTSerial2.flush();
-  BTSerial.flush();
-  Serial.flush();
-
-  BTSerial.listen();
-
-  
-}
-
-void loop(){
-  
-  if (BTSerial.available()) {
-    Serial.println("testing");
-    command = BTSerial.readString();
-    //delay(500);
-    Serial.println("Received: " + command);
-    //BTSerial.flush();
-  }
-
-  command.trim();
+void writingCommands(String command){
   int index;
-
   while ((index = command.indexOf(' ')) != -1) {
     // Extract the word before the space
     String word = command.substring(0, index);
@@ -267,4 +222,47 @@ void loop(){
     commands.clear();
   }
   command = "";
+}
+
+
+void setup()
+{
+  // Pin setup for IO
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  pinMode(rxPin2, INPUT);
+  pinMode(txPin2, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(masterPowerPin, OUTPUT);
+
+  // Begin the regular serial communication at 9600 baud rate (for debugging)
+  Serial.begin(9600);
+  BTSerial.begin(9600); // Now start Bluetooth communication
+  BTSerial2.begin(9600);
+  delay(100);
+  // Print to Serial Monitor to check that the program is running
+  BTSerial2.flush();
+  BTSerial.flush();
+  Serial.flush();
+
+  BTSerial.listen();
+  
+
+  
+}
+
+void loop(){
+  if (BTSerial.available()) {
+    //Serial.println("testing");
+    command = BTSerial.readString();
+    //delay(500);
+    Serial.println("Received: " + command);
+    //BTSerial.flush();
+  }
+
+  command.trim();
+  writingCommands(command);
+  command = "";
+  
 }
